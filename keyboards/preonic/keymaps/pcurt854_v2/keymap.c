164a4c7cd2fee32dbf56cb705b5ca33b1684e12a
Pete Curt <pcurt854@gmail.com>
2020-12-06 22:42:21 -0500

    Added return false after each bracket pair

--- keyboards/preonic/keymaps/pcurt854_v2/keymap.c
+++ keyboards/preonic/keymaps/pcurt854_v2/keymap.c
@@ -293,21 +293,25 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
           if (record->event.pressed) {
             SEND_STRING("<>"SS_TAP(X_LEFT));
           }
+          return false;
           break;
         case RBKPAIR:
           if (record->event.pressed) {
             SEND_STRING("()"SS_TAP(X_LEFT));
           }
+          return false;
           break;
         case SBKPAIR:
           if (record->event.pressed) {
             SEND_STRING("[]"SS_TAP(X_LEFT));
           }
+          return false;
           break;
         case CBKPAIR:
           if (record->event.pressed) {
             SEND_STRING("{}"SS_TAP(X_LEFT));
           }
+          return false;
           break;
         case VLKTOGG:
           if (record->event.pressed) {
