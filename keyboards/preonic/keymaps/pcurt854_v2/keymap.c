5c250f767a1ecd3024e5a71a97c73dd4b9cff5d5
Pete Curt <pcurt854@gmail.com>
2021-01-03 16:49:49 -0500

    Turned off beeping when no higher layer was on

--- keyboards/preonic/keymaps/pcurt854_v2/keymap.c
+++ keyboards/preonic/keymaps/pcurt854_v2/keymap.c
@@ -67,7 +67,8 @@ float higher_layer_on_songs[][1][2] = {
   SONG(LAYER5_SONG),
   SONG(LAYER6_SONG)
 };
-float higher_layer_off_song[][2] = SONG(GOODBYE_SOUND);
+// float higher_layer_off_song[][2] = SONG(GOODBYE_SOUND);
+float higher_layer_off_song[][2] = SONG(NO_SOUND);
 
 #define ONESHOT_SHIFT_ON_SONG H__NOTE(_C4), H__NOTE(_C6),
 #define ONESHOT_SHIFT_OFF_SONG H__NOTE(_C6), H__NOTE(_C4),
