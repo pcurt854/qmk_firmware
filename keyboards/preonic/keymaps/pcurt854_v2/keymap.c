e8c1b1c172c5911d3d96155fa34f67581ade91f1
Pete Curt <pcurt854@gmail.com>
2021-07-27 14:34:58 -0400

    Moved the bracket pairs under the index finger on Layer 5
    and moved the separate ones around the pair.
    This actually produced more empty keys on the layer.

--- keyboards/preonic/keymaps/pcurt854_v2/keymap.c
+++ keyboards/preonic/keymaps/pcurt854_v2/keymap.c
@@ -247,25 +247,25 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 
 /* Bracket layer 5
- * ,-----------------------------------------------------------------------------------.
- * |      |gradnt|plain | test |      |      |  <|>  |    < | >    |  <|>  |      | del  |
- * |      |      |      |      |      |      |       |      |      |       |      | brks |
- * |------+------+------+------+------+--------------+------+------+-------+------|------|
- * |      | Xmas |plain |plain | Rgb  | saT+ |  (|)  |    ( | )    |  (|)  |      | ____ |
- * |------+------+------+------+------+--------------+------+------+-------+------|------|
- * |      |swirl |rainbw|breath| hue+ | mod+ |  [|]  |    [ | ]    |  [|]  |      |      |
- * |------+------+------+------+------+------|-------+------+------+-------+------|------|
- * |      |plain |snake |knight| vlk  | Brt+ |  {|}  |    { | }    |  {|}  | home |      |
- * |------+------+------+------+------+------+-------+------+------+-------+------+------|
- * | ____ | ____ | ____ | ____ |      |      |/\*|*\/|  /\* | *\/  |/\*|*\/| end  |      |
+ * ,-------------------------------------------------------------------------------------.
+ * |      |gradnt|plain | test |      |      |  <  |   <|>  |  >   |       |      | del  |
+ * |      |      |      |      |      |      |     |        |      |       |      | brks |
+ * |------+------+------+------+------+------------+--------+------+-------+------|------|
+ * |      | Xmas |plain |plain | Rgb  | saT+ |  (  |   (|)  |  )   |       |      | ____ |
+ * |------+------+------+------+------+------------+--------+------+-------+------|------|
+ * |      |swirl |rainbw|breath| hue+ | mod+ |  [  |   [|]  |  ]   |       |      |      |
+ * |------+------+------+------+------+------|-----+--------+------+-------+------|------|
+ * |      |plain |snake |knight| vlk  | Brt+ |  {  |   {|}  |  }   |       | home |      |
+ * |------+------+------+------+------+------+-----+--------+------+-------+------+------|
+ * | ____ | ____ | ____ | ____ |      |      | /\* | /\*|*\/| *\/  |       | end  |      |
  * `------------------------------------------------------------------------------------'
  */
 [_BRACKET] = LAYOUT_preonic_grid(
-  XXXXXXX, RGB_M_G,  RGB_M_P,  RGB_M_T, XXXXXXX, XXXXXXX, ABKPAIR, KC_LABK, KC_RABK, ABKPAIR, XXXXXXX, DBKPAIR,
-  XXXXXXX, RGB_M_X,  RGB_M_P,  RGB_M_P, RGB_TOG, RGB_SAI, RBKPAIR, KC_LPRN, KC_RPRN, RBKPAIR, XXXXXXX, _______,
-  XXXXXXX, RGB_M_SW, RGB_M_R,  RGB_M_B, RGB_HUI, RGB_MOD, SBKPAIR, KC_LBRC, KC_RBRC, SBKPAIR, XXXXXXX, XXXXXXX,
-  XXXXXXX, RGB_M_P,  RGB_M_SN, RGB_M_K, VLKTOGG, RGB_VAI, CBKPAIR, KC_LCBR, KC_RCBR, CBKPAIR, KC_HOME, XXXXXXX,
-  _______, _______,  _______,  _______, XXXXXXX, XXXXXXX, CCMT,    CCMTLT,  CCMTRT,  CCMT,    KC_END,  XXXXXXX
+  XXXXXXX, RGB_M_G,  RGB_M_P,  RGB_M_T, XXXXXXX, XXXXXXX, KC_LABK, ABKPAIR, KC_RABK, XXXXXXX, XXXXXXX, DBKPAIR,
+  XXXXXXX, RGB_M_X,  RGB_M_P,  RGB_M_P, RGB_TOG, RGB_SAI, KC_LPRN, RBKPAIR, KC_RPRN, XXXXXXX, XXXXXXX, _______,
+  XXXXXXX, RGB_M_SW, RGB_M_R,  RGB_M_B, RGB_HUI, RGB_MOD, KC_LBRC, SBKPAIR, KC_RBRC, XXXXXXX, XXXXXXX, XXXXXXX,
+  XXXXXXX, RGB_M_P,  RGB_M_SN, RGB_M_K, VLKTOGG, RGB_VAI, KC_LCBR, CBKPAIR, KC_RCBR, XXXXXXX, KC_HOME, XXXXXXX,
+  _______, _______,  _______,  _______, XXXXXXX, XXXXXXX, CCMTLT,  CCMT,    CCMTRT,  XXXXXXX, KC_END,  XXXXXXX
 ),
 
 /* Adjust layer 6
