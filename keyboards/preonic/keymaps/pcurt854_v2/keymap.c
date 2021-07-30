2f94861f3966ccc3f2a86e225ca2ec7fcee385eb
Pete Curt <pcurt854@gmail.com>
2021-07-30 02:48:32 -0400

    Removed Yields from Layer 4

--- keyboards/preonic/keymaps/pcurt854_v2/keymap.c
+++ keyboards/preonic/keymaps/pcurt854_v2/keymap.c
@@ -228,7 +228,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------------------------------------------------.
  * |      |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |  F7  |  F8  |  F9  |  F10 | ____ |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | ____ |      |      |   !  |   $  |   ~  |. -> .|   _  |   |  |/\**\/|   %  |      |
+ * | ____ |      |      |   !  |   $  |   ~  |      |   _  |   |  |/\**\/|   %  |      |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * | ____ |   @  |   *  |  {}  |      |   `  |   #  |   -  |  ->  |   +  |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
@@ -239,7 +239,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_SYMBOL] = LAYOUT_preonic_grid(
   XXXXXXX, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______,
-  _______, XXXXXXX, XXXXXXX, KC_EXLM, KC_DLR,  KC_TILD, YIELDS,  KC_UNDS, KC_PIPE, CCMT,    KC_PERC, XXXXXXX,
+  _______, XXXXXXX, XXXXXXX, KC_EXLM, KC_DLR,  KC_TILD, XXXXXXX, KC_UNDS, KC_PIPE, CCMT,    KC_PERC, XXXXXXX,
   _______, KC_AT,   KC_ASTR, CBKPAIR, XXXXXXX, KC_GRV,  KC_HASH, KC_MINS, ARROW,   KC_PLUS, XXXXXXX, XXXXXXX,
   XXXXXXX, SBKPAIR, RBKPAIR, KC_CIRC, SCMD(LOPT(KC_V)),
                                                KC_BSLS, KC_AMPR, KC_EQL,  ABKPAIR, DOTSLSH, KC_PGUP, XXXXXXX,
