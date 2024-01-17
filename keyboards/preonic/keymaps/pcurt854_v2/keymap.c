ae639c4321cecdd94d3860677b2cfce1f03e5875
Pete Curt <pcurt854@gmail.com>
2024-01-17 16:01:05 -0500

    Removed OSM front the lower left Shift key

--- keyboards/preonic/keymaps/pcurt854_v2/keymap.c
+++ keyboards/preonic/keymaps/pcurt854_v2/keymap.c
@@ -132,8 +132,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
            KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, RCTL_T(KC_QUOT),
   MO(_BRACKET),
            KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP,   LT(_ADJUST,KC_SLSH),
-  OSM(MOD_LSFT),
-           KC_LCTL, KC_LOPT, KC_LCMD, MO(_SYMBOL),
+  KC_LSFT, KC_LCTL, KC_LOPT, KC_LCMD, MO(_SYMBOL),
                                                KC_ENT,  KC_SPC,  TT(_NUMPAD),
                                                                           KC_RCMD, KC_LEFT, KC_DOWN, KC_RGHT
 ),
